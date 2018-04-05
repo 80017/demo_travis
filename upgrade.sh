210e839a6132a7c15e4b2e19f5007665c0040fa8
80017 <arpitap@officebeacon.com>
2018-04-05 23:22:13 +0530

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -4,4 +4,4 @@ curl -u ""$RANCHER_ACCESSKEY":"$RANCHER_SECRETKEY"" \
 -H 'Content-Type: application/json' \
 -d '{
      "inServiceStrategy":{"launchConfig": {"imageUuid":"docker:80017/demo_travis:dev","kind": "container","labels":{"io.rancher.container.pull_image": "always","io.rancher.scheduler.affinity:host_label": "machine=elastic-server"},"ports": ["5555:5555/tcp"],"healthCheck": {"type": "instanceHealthCheck","healthyThreshold": 2,"initializingTimeout": 60000,"interval": 2000,"name": null,"port": 5555,"recreateOnQuorumStrategyConfig": {"type": "recreateOnQuorumStrategyConfig","quorum": 1},"reinitializingTimeout": 60000,"responseTimeout": 60000,"strategy": "recreateOnQuorum","unhealthyThreshold": 3},"networkMode": "managed"}},"toServiceStrategy":null}' \
-http://rancher.flowz.com:8080/v2-beta/projects/1a20750/services/$SERVICE_ID?action=upgrade
+http://rancher.flowz.com:8080/v2-beta/projects/1a20750/services/1s1340?action=upgrade
