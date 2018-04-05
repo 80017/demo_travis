e3bc4bd79ceed77902d7d04f04822e876b4f8a66
arpita patel <arpitap@officebeacon.com>
2018-04-05 23:13:25 +0530

    first commit

---
+++ finish.sh
@@ -0,0 +1,25 @@
+echo "waiting for service to upgrade "
+    while true; do
+
+      case `curl -u ""$RANCHER_ACCESSKEY":"$RANCHER_SECRETKEY"" \
+          -X GET \
+          -H 'Accept: application/json' \
+          -H 'Content-Type: application/json' \
+          "http://rancher.flowz.com:8080/v2-beta/projects/1a20750/services/$SERVICE_ID/" | jq '.state'` in
+          "\"upgraded\"" )
+              echo "completing service upgrade"
+              curl -u ""$RANCHER_ACCESSKEY":"$RANCHER_SECRETKEY"" \
+                -X POST \
+                -H 'Accept: application/json' \
+                -H 'Content-Type: application/json' \
+                "http://rancher.flowz.com:8080/v2-beta/projects/1a20750/services/$SERVICE_ID?action=finishupgrade"
+              break ;;
+          "\"upgrading\"" )
+              echo "still upgrading"
+              echo -n "."
+              sleep 60
+              continue ;;
+          *)
+              die "unexpected upgrade state" ;;
+      esac
+    done
