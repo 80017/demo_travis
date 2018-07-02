22011ac21df81f60a63c301618627ca2fb712536
80017 <arpitapatel0611@gmail.com>
2018-07-02 12:17:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 'use strict';
 
 const express = require('express');
+var fs = require('fs');
 
 const PORT = 5555;
 
 const app = express();
 
 app.get('/',function (req, res) {
- res.send('Welcome To FLOWZ...................');
+ 
+ var img = fs.readFileSync('./ob.png');
+	res.writeHead(200, {'Content-Type': 'image/png' });
+     	res.end(img, 'binary');
+ 
+# res.send('Welcome To FLOWZ...................');
 });
 
 app.listen(PORT);
