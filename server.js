6620863a8f26fbbf2ad9fff6adf59d2a3c28653c
80017 <arpitapatel0611@gmail.com>
2018-07-02 14:08:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,31 +1,18 @@
 'use strict';
 
-//const express = require('express');
-//const PORT = 5555;
+const express = require('express');
+const PORT = 5555;
 
-//const app = express();
+const app = express();
 
-//app.get('/',function (req, res) {
-//   res.send('Welcome To FLOWZ...................');
-//});
+app.get('/',function (req, res) {
+   res.send('Welcome To FLOWZ...................');
+});
 
-//app.listen(PORT);
-//console.log('port is:' + PORT);
-
-
+app.listen(PORT);
+console.log('port is:' + PORT);
 
 
 
-var express = require('express');
-var fs = require('fs');
 
-var app = express()
- 
-app.get('/', function (req, res) {
-	var img = fs.readFileSync('./ob.png');
-	res.writeHead(200, {'Content-Type': 'image/png' });
-     	res.end(img, 'binary');
-  	// res.send('Hello World')
-})
- 
-app.listen(5555)
+
