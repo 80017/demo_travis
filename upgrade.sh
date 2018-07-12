e076361c514ef979331e5695809756e53e4b6dcf
80017 <arpitapatel0611@gmail.com>
2018-07-12 19:48:47 +0530

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -1,7 +1,6 @@
-curl -u ""$RANCHER_ACCESSKEY":"$RANCHER_SECRETKEY"" \
--X POST \
+curl -X POST \
 -H 'Accept: application/json' \
 -H 'Content-Type: application/json' \
 -d '{
-     "inServiceStrategy":{"launchConfig": {"imageUuid":"docker:80017/demo_travis:dev","kind": "container","labels":{"io.rancher.container.pull_image": "always","io.rancher.scheduler.affinity:host_label": "machine=elastic-server"},"ports": ["5555:5555/tcp"],"healthCheck": {"type": "instanceHealthCheck","healthyThreshold": 2,"initializingTimeout": 60000,"interval": 2000,"name": null,"port": 5555,"recreateOnQuorumStrategyConfig": {"type": "recreateOnQuorumStrategyConfig","quorum": 1},"reinitializingTimeout": 60000,"responseTimeout": 60000,"strategy": "recreateOnQuorum","unhealthyThreshold": 3},"networkMode": "managed"}},"toServiceStrategy":null}' \
-http://rancher.flowz.com:8080/v2-beta/projects/1a20750/services/1s1340?action=upgrade
+     "inServiceStrategy":{"launchConfig": {"imageUuid":"docker:80017/demo_travis:dev","kind": "container","labels":{"io.rancher.container.pull_image": "always"},"ports": ["5555:5555/tcp"],"healthCheck": {"type": "instanceHealthCheck","healthyThreshold": 2,"initializingTimeout": 60000,"interval": 2000,"name": null,"port": 5555,"recreateOnQuorumStrategyConfig": {"type": "recreateOnQuorumStrategyConfig","quorum": 1},"reinitializingTimeout": 60000,"responseTimeout": 60000,"strategy": "recreateOnQuorum","unhealthyThreshold": 3},"networkMode": "managed"}},"toServiceStrategy":null}' \
+http://172.16.61.176:8080/v2-beta/projects/1a5/services/1s84?action=upgrade
